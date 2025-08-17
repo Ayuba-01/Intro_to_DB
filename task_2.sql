a46ed6c7236d8e4e5c17625190c983eae8479775
ayuba <yusufayuba.a11@gmail.com>
2025-08-17 01:06:11 +0100

    Create Your Magical Tables

---
+++ task_2.sql
@@ -0,0 +1,45 @@
+-- Active: 1741625150764@@127.0.0.1@5432
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
+CREATE TABLE IF NOT EXISTS authors (
+  author_id INT NOT NULL AUTO_INCREMENT,
+  author_name VARCHAR(215),
+  PRIMARY KEY (author_id)
+);
+
+CREATE TABLE IF NOT EXISTS customers (
+  customer_id INT NOT NULL AUTO_INCREMENT,
+  customer_name VARCHAR(215),
+  email VARCHAR(215),
+  address TEXT,
+  PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS books (
+  book_id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(130),
+  author_id INT,
+  price DOUBLE,
+  publication_date DATE,
+  PRIMARY KEY (book_id),
+  FOREIGN KEY (author_id) REFERENCES authors (author_id)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+  order_id INT NOT NULL AUTO_INCREMENT,
+  customer_id INT,
+  order_date DATE,
+  PRIMARY KEY (order_id),
+  FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS `order details` (
+  orderdetailid INT NOT NULL AUTO_INCREMENT,
+  order_id INT,
+  book_id INT,
+  quantity DOUBLE,
+  PRIMARY KEY (orderdetailid),
+  FOREIGN KEY (order_id) REFERENCES orders (order_id),
+  FOREIGN KEY (book_id) REFERENCES books (book_id)
+);
